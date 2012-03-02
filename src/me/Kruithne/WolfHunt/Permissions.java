ece75a88ae922dde50e7261a0b58f10d824f2199
Kruithne <personal@stanleybatch.co.uk>
2012-03-02 22:54:20 +0000

    Added missing files from last commit

---
+++ src/me/Kruithne/WolfHunt/Permissions.java
@@ -0,0 +1,11 @@
+package me.Kruithne.WolfHunt;
+
+public class Permissions {
+
+	public static String permissionFormat = "wolfhunt.%s";
+	public static String canTrack = "canTrack";
+	public static String commandSpawnWolf = "canSpawnWolf";
+	public static String commandGetConfig = "canGetConfig";
+	public static String commandSetConfig = "canSetConfig";
+	
+}
---
+++ src/me/Kruithne/WolfHunt/VanishHandler.java
@@ -0,0 +1,12 @@
+package me.Kruithne.WolfHunt;
+
+public class VanishHandler {
+
+	private WolfHunt plugin;
+	
+	VanishHandler(WolfHunt plugin)
+	{
+		this.plugin = plugin;
+	}
+	
+}
