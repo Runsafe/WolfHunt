d576a80c0b802f8031966a800859c83e3d58a205
Kruithne <personal@stanleybatch.co.uk>
2012-03-02 17:54:58 +0000

    Edited typo in command information

--- src/me/Kruithne/WolfHunt/Constants.java
+++ src/me/Kruithne/WolfHunt/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
 	public static String commandInfoSpawnWolf = String.format(Constants.commandInfoFormat, "spawnwolf", "Spawns an untamed wolf");
 	public static String commandInfoGetConfig = String.format(Constants.commandInfoFormat, "getconfig [key]", "Gets a config value");
 	public static String commandInfoSetConfig = String.format(Constants.commandInfoFormat, "getconfig [key] [value]", "Sets a config value");
-	public static String commandInfoReloadConfig = String.format(Constants.commandInfoFormat, "reload", "Reloads configuration from file");
+	public static String commandInfoReloadConfig = String.format(Constants.commandInfoFormat, "reloadconfig", "Reloads configuration from file");
 	
 	public static String commandInfoConfigNoExists = "That config value does not exist in WolfHunt";
 	public static String commandInfoGetConfigReturnFormat = "WolfHunt Config: %s = %s";
