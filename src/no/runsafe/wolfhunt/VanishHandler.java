db46ac456a9961b7b44c0a5db80fbc67815f2bf2
Morten Nilsen <morten@runsafe.no>
2013-12-20 14:54:58 +0100

    Remove direct reliance on VanishNoPacket

--- src/no/runsafe/wolfhunt/VanishHandler.java
+++ src/no/runsafe/wolfhunt/VanishHandler.java
@@ -1,50 +1,21 @@
 package no.runsafe.wolfhunt;
 
 import no.runsafe.framework.api.player.IPlayer;
-import org.bukkit.Server;
-import org.bukkit.entity.Player;
-import org.kitteh.vanish.VanishManager;
-import org.kitteh.vanish.VanishPlugin;
 
-public class VanishHandler {
-
-	private Server server;
+public class VanishHandler
+{
 	private Configuration config;
-	private VanishPlugin vanish = null;
-	private VanishManager vanishManager = null;
-	
-	private boolean initiated = false;
-	
-	VanishHandler(Server server, Configuration config)
+
+	VanishHandler(Configuration config)
 	{
-		this.server = server;
 		this.config = config;
 	}
-	
+
 	public boolean playerIsVanished(IPlayer player)
 	{
-		if(!config.enableVanishNoPacketSupport)
+		if (!config.enableVanishNoPacketSupport)
 			return false;
 
-		VanishManager manager = getVanishManager();
-		return manager != null && manager.isVanished(player);
+		return player.isVanished();
 	}
-
-	private VanishManager getVanishManager()
-	{
-		if(vanishManager != null)
-			return vanishManager;
-			
-		if(vanish == null && !this.initiated)
-		{
-			vanish = (VanishPlugin) this.server.getPluginManager().getPlugin("VanishNoPacket");
-			this.initiated = true;
-		}
-	
-		if(vanish != null && vanish.isEnabled())
-			vanishManager = this.vanish.getManager();
-	
-		return vanishManager;
-	}
-	
 }
