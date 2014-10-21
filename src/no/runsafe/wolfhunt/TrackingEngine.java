0023c85018e55ed3f3bcc6621986d93d3b7fab1f
Kruithne <kruithne@gmail.com>
2014-10-22 00:45:10 +0100

    Fix the lore text for the blood vial that players drop.

--- src/no/runsafe/wolfhunt/TrackingEngine.java
+++ src/no/runsafe/wolfhunt/TrackingEngine.java
@@ -132,7 +132,7 @@ public class TrackingEngine implements IPlayerInteractEntityEvent, IPlayerDeathE
 			RunsafeMeta vial = Item.Brewing.Potion.getItem();
 			vial.setDurability((short) 8261);
 			vial.setDisplayName("§3Vial of Blood");
-			vial.addLore("§7" + player.getName());
+			vial.addLore("§7Track: " + player.getName());
 
 			world.dropItem(location, vial);
 		}
